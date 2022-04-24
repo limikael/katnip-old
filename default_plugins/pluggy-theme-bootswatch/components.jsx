1a58d9bc332e9c46f15a079ef8c77e77da3f8954
Mikael Lindqvist <li.mikael@gmail.com>
2022-04-24 16:23:48 +0800

    menusgit add .!

---
+++ default_plugins/pluggy-theme-bootswatch/components.jsx
@@ -0,0 +1,61 @@
+import {A, pluggy} from "pluggy";
+
+function Nav({request}) {
+	let [session, setSession]=pluggy.useSession();
+
+	return (
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div class="container-fluid">
+				<A class="navbar-brand" href="/">My Site</A>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarColor01">
+					<ul class="navbar-nav me-auto">
+						{session.menuHeader.map(item=>{
+							let cls="nav-link";
+
+							if (request.path==item.href)
+								cls+=" active";
+
+							return (
+								<li class="nav-item">
+									<A class={cls} href={item.href}>
+										{item.label}
+									</A>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export function PageTemplate({request,children}) {
+	return (
+		<>
+			<link 
+				rel="stylesheet" 
+				href="/public/bootstrap-minty.min.css"
+			/>
+			<script
+				src="/public/bootstrap.bundle.min.js"
+				async
+			/>
+			<style global jsx>{`
+				html, body, .page {
+					height: 100%;
+				}
+			`}</style>
+			<div class="page d-flex flex-column">
+				<Nav request={request}/>
+				<div class="container mt-3 flex-grow-1">
+					{children}
+				</div>
+			</div>
+		</>
+	);
+}
--- default_plugins/pluggy-theme-bootswatch/index.jsx
+++ default_plugins/pluggy-theme-bootswatch/index.jsx
@@ -1,15 +1,5 @@
-import PageTemplate from "./components/PageTemplate.jsx";
-import BootswatchAdmin from "./components/BootswatchAdmin.jsx";
-import NotFound from "./components/NotFound.jsx";
 import pluggy from "pluggy";
-
-pluggy.addAction("getAdminMenu",(items)=>{
-	items.push({
-		title: "Bootswatch",
-		href: "/admin/bootswatch",
-		priority: 20
-	});
-})
+import {PageTemplate} from "./components.jsx";
 
 pluggy.addAction("getMenuLocations",(items)=>{
 	items.push({
@@ -23,12 +13,11 @@ pluggy.addAction("getMenuLocations",(items)=>{
 	});
 })
 
-pluggy.addAction("getPageComponent",(request)=>{
-	if (request.path=="/admin/bootswatch")
-		return BootswatchAdmin;
-});
-
 pluggy.addAction("getPageTemplate",(request)=>{
 	if (request.params[0]!="admin")
 		return PageTemplate;
+});
+
+pluggy.addAction("getClientSession",async (clientSession)=>{
+	clientSession.menuHeader=pluggy.getSetting("menuHeader");
 });
