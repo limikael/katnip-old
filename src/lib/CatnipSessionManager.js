96f70f30026de423de2bc2211a8dc5ce1a2cdb76
Mikael Lindqvist <li.mikael@gmail.com>
2022-05-13 13:26:46 +0800

    rm log

--- src/lib/CatnipSessionManager.js
+++ src/lib/CatnipSessionManager.js
@@ -23,7 +23,6 @@ export default class CatnipSessionManager {
 			this.clientSession,
 			(newSession)=>{
 				Object.assign(this.clientSession,newSession);
-				console.log(this.clientSession);
 				forceUpdate();
 			}
 		]
