26897da44a76431add77c2f063060c835abfbed8
Mikael Lindqvist <li.mikael@gmail.com>
2022-10-25 01:58:45 +0800

    remove debug el

--- default_plugins/katnip-pages/src/katnip-pages-browser.jsx
+++ default_plugins/katnip-pages/src/katnip-pages-browser.jsx
@@ -1,11 +1,10 @@
-import {katnip, convertToSlug, useChannel, A} from "katnip";
+import {katnip} from "katnip";
 import PageView from "../components/PageView.jsx";
 import PageAdmin from "../components/PageAdmin.jsx";
-import FILE_EARMARK_TEXT from "bootstrap-icons/icons/file-earmark-text.svg";
 import "./standard-elements.jsx";
 
 //import "../components/PageCounter.jsx";
-import "../components/Box.jsx";
+//import "../components/Box.jsx";
 
 katnip.addAction("getAdminMenu",(items)=>{
 	items.push({
